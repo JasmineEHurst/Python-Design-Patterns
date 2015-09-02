516d3aec03d1f74aee645ee25a483d911c374225
Jasmine Hurst <jasmine.ehurst@gmail.com>
2015-09-01 22:53:40 -0500

    Factory example complete

--- factory.py
+++ factory.py
@@ -1,1 +1,32 @@
 __author__ = 'Jazz'
+#Simple dog class
+class Dog:
+
+    def __init__(self,name):
+        self.name = name
+
+    def speak(self):
+        return "Woof!"
+
+class Cat:
+
+    def __init__(self,name):
+        self.name = name
+
+    def speak(self):
+        return "Meow!"
+
+def get_pet(pet="dog"):
+    #factory method
+    pets = dict(dog=Dog("RJ"),cat=Cat("Sammi"))
+    return pets[pet]
+
+
+d = get_pet("dog")
+
+print(d.speak())
+
+c = get_pet("cat")
+
+print(c.speak())
+
